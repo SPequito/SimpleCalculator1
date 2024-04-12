370378f65ee69beacc0576c511ea915d15b45bd0
stefan <stefan.almeida@gmail.com>
2024-04-12 12:07:29 +1200

    some changes, like introduction multiplier and divider functions, put a backgroud and change the layout, and introduction another listener to a more visual

--- app/src/main/java/com/codecademy/simplecalculator/MainActivity.java
+++ app/src/main/java/com/codecademy/simplecalculator/MainActivity.java
@@ -25,28 +25,60 @@ public class MainActivity extends AppCompatActivity {
         final RadioGroup operators = findViewById(R.id.operators);
         final RadioButton add = findViewById(R.id.radioButtonAdd);
         final RadioButton sub = findViewById(R.id.radioButtonSub);
+        final RadioButton mult = findViewById(R.id.radioButtonMult);
+        final RadioButton div = findViewById(R.id.radioButtonDiv);
         final Button equal = findViewById(R.id.buttonEqual);
         final TextView finalresult = findViewById(R.id.result);
+        final TextView selectOperator = findViewById(R.id.textViewSelectOperator);
 
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               final int textNumberOne = Integer.parseInt(firstNumber.getText().toString());
-               final int textNumberTwo = Integer.parseInt(secondNumber.getText().toString());
+               final double textNumberOne = Double.parseDouble(firstNumber.getText().toString());
+               final double textNumberTwo = Double.parseDouble(secondNumber.getText().toString());
                final int operatorId = operators.getCheckedRadioButtonId();
-               Integer result = 0;
-               //were
+               double result = 0;
+
+// calculation logic functions
                     if(operatorId == add.getId()){
 
                         result = textNumberOne + textNumberTwo;
 
                         }else if(operatorId == sub.getId()) {
 
-                        result = textNumberOne - textNumberTwo;
+                            result = textNumberOne - textNumberTwo;
 
-                        }
-                finalresult.setText(result.toString());
+                                }else if(operatorId == mult.getId()) {
+
+                                    result = textNumberOne * textNumberTwo;
+
+                                        }else if(operatorId == div.getId()) {
+
+                                            result = textNumberOne / textNumberTwo;
+
+                    }
+
+                finalresult.setText(Double.toString(result));
             }
+        });
+
+        // after i press my operator we can see a visual change, for that i use the listener for wend one of the operator is press, set the textfilt we have in the middle
+        operators.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                if(add.isPressed()){
+                    selectOperator.setText("+");
+                    }else if(sub.isPressed()){
+                        selectOperator.setText("-");
+                        }else if(mult.isPressed()){
+                            selectOperator.setText("x");
+                        }else if(div.isPressed()){
+                            selectOperator.setText(":");
+                }
+            }
+
+
         });
 
     }
